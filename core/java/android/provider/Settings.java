5ba11a698736956d57b3191e2c4a8d39bd595ef5
xplodwild <xplod@ouverta.fr>
2013-10-16 16:28:17 -0400

    [1/2] Add battery level around unlock ring
    PS1: squash conflicts
    PS2: Untested at the moment
    PS3: Works well
    PS4: Attempt to only work on expanded desktop/pie mode
    
    Signed-off-by: Ayysir <dresadd09691@gmail.com>
    
    Change-Id: I076307a0d6bf80b1fc7961cb7520a960818fba5c
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3268,6 +3268,12 @@ public final class Settings {
          */
 	public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
 
+        /**
+         * Whether or not to show circle battery around the lockscreen ring
+         * @hide
+         */
+        public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
+        
 	/**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
